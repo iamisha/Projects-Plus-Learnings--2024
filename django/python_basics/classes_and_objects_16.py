04c1fff89f6d4332d1c5fa0a30d671c178b8bf77
Isha Hitang <isha.201710@ncit.edu.np>
2024-11-10 09:55:57 +0545

    testing...

--- django/python_basics/classes_and_objects_16.py
+++ django/python_basics/classes_and_objects_16.py
@@ -7,14 +7,34 @@
 # o1 = MyClass()
 # print(o1.x)
 
+
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# # o2 = Person('Kichu', 24)
+
+# name = input('Enter your name: ')
+# age = int(input('Enter your age: '))
+# o2 = Person(name, age)
+# print('Your name is '+o2.name + ' and your age is ',o2.age)
+
+
+
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-# o2 = Person('Kichu', 24)
+o2 = Person('Kichu', 24)
 
-name = input('Enter your name: ')
-age = int(input('Enter your age: '))
-o2 = Person(name, age)
-print('Your name is '+o2.name + ' and your age is ',o2.age)
+del o2.age
+
+# gives an error 'object has no attribute age' as it has been deleted earlier
+
+print(o2.age)
+del o2.name
+
+
