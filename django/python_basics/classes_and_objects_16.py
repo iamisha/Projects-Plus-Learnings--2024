4aaa62100e2001b9cefb39af8ab734738f44aa0b
Isha Hitang <isha.201710@ncit.edu.np>
2024-11-10 09:48:52 +0545

    testing...

---
+++ django/python_basics/classes_and_objects_16.py
@@ -0,0 +1,20 @@
+# python is an object oriented programming language, anything
+# that deals with classes, objects
+
+# class MyClass:
+#     x = 5
+
+# o1 = MyClass()
+# print(o1.x)
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# o2 = Person('Kichu', 24)
+
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+o2 = Person(name, age)
+print('Your name is '+o2.name + ' and your age is ',o2.age)
