e128639c2fc697067f0bf25afff352ddc26c5b1e
Isha Hitang <isha.201710@ncit.edu.np>
2024-11-10 10:03:14 +0545

    10th Nov. 2024 MS

--- django/python_basics/classes_and_objects_16.py
+++ django/python_basics/classes_and_objects_16.py
@@ -23,24 +23,24 @@
 
 
 
-# class Person:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-# o2 = Person('Kichu', 24)
+o2 = Person('Kichu', 24)
 
-# del o2.age
+del o2.age
 
-# # gives an error 'object has no attribute age' as it has been deleted earlier
+# gives an error 'object has no attribute age' as it has been deleted earlier
 
-# print(o2.age)
-# del o2.name
+print(o2.age)
+del o2.name
 
 
 # let's understand the function of pass in python class
 
 # without using pass it gives an error
-class Person:
-    pass # often used as a placeholder
+# class Person:
+#     pass # often used as a placeholder
 
